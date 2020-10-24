3da0cb17f598059ce4f04e2eeb7e36c24193ca3a
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-24 13:42:01 +0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
