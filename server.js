7945c07ef1dd1548c8102cc7e97a0b0ace85ea66
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-24 13:47:50 +0700

    Hook up Socket.io from server to client

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connect
+io.on('connection', socket => {
+  console.log('New web socket connection...');
+
+  socket.emit('message', 'Welcome to ChatCord!');
+
+  
+});
 
 const PORT = 3000 || process.env.PORT;
 
