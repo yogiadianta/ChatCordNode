ce311bb035dce4e01750585a9758ac6d86c8a7e0
Yogi Adianta <yogiadianta0@gmail.com>
2020-10-24 17:56:23 +0700

    Add user join and leave in chat

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessages = require('./utils/messages');
-const { userJoin, getCurrentUser} = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -36,7 +36,11 @@ io.on('connection', socket => {
   
   // Runs when clients disconnects
   socket.on('disconnect', () => {
-    io.emit('message', formatMessages(botName, 'A user has left the chat'));
+    const user = userLeave(socket.id);
+
+    if (user){
+      io.to(user.room).emit('message', formatMessages(botName, `${user.username} has left the chat`));
+    }
   });
 });
 
